b6e20fe8b9880a598dbf73775943dce8a90db52f
Aarav Panday <aaravpanday4@gmail.com>
2026-07-16 21:16:01 +0545

    for hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Aarkoday.github.io',
+  base: '/RBSMUN-WEBSITE',
   integrations: [react(), tailwind()],
 });
